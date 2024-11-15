c0a58e61ecb77bf5d42a577e2fcf434bc0d60d9d
Alex Stranc <aleksander_stranc@sluz.ch>
2024-11-15 09:34:25 +0100

    Ballerspiel fertig

--- Ballerspiel/Baelle.js
+++ Ballerspiel/Baelle.js
@@ -6,12 +6,20 @@ document.getElementById("ball4").addEventListener("click", changePicture);
 function changePicture(e) {  
 
     var bild = e.target;
-
+    
     if (bild.src.indexOf("images/ball.png") > -1) { 
         bild.src = "images/kreuz.png";
     }
-    
+
     else if (bild.src.indexOf("images/kreuz.png") > -1) {
         bild.remove();
     }
+
+    var remainingPictures = document.querySelectorAll("img");
+    var ImageCount = remainingPictures.length;
+
+    if (ImageCount === 0){
+        document.getElementById("gameover").innerText = "GAME OVER";
+    }
+        
 }
